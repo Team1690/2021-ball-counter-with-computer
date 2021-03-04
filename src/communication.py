4ac25cb5177af2a3093f210460ec29927ec07a55
IdoSobol821 <idosobol1005@gmail.com>
2021-03-04 21:03:34 +0200

    basic communication with json

--- src/communication.py
+++ src/communication.py
@@ -1,6 +1,15 @@
 import serial
-ser = serial.Serial('COM3', 9600)
-ser.reset_input_buffer()
+import json
 
-ser.write(b'H')
-ser.close()
+bluetooth = serial.Serial('COM3', 9600)
+bluetooth.reset_input_buffer()
+
+# bluetooth.write(b'H')
+input = bluetooth.read(22)
+print(input)
+input_json = json.loads(input)
+print(input_json["value"])
+print("------------")
+print(input_json["value2"])
+
+bluetooth.close()
