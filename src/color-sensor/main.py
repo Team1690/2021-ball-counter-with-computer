865d399ce9cfb92b02876f741ab8964a9c1cbc5a
Yonatan Tzury <ytzury@gmail.com>
2021-07-09 17:32:51 +0300

    Fixed FMS data sending.

--- src/color-sensor/main.py
+++ src/color-sensor/main.py
@@ -10,6 +10,8 @@ from requests.exceptions import ConnectTimeout
 import time
 import _thread as thread
 import websocket
+import json
+import colorsys
 
 FMS_SERVER = "10.0.100.5:8080"
 ALLIANCE_COLOR = 'red' # Change accordingly
@@ -19,6 +21,13 @@ S2_PIN = 19
 S3_PIN = 13
 OUTPUT_PIN = 26
 CYCLES_TO_WAIT = 3
+
+COLORS = {
+    "blue": {"prev": "yellow", "next": "green"},
+    "green": {"prev": "blue", "next": "red"},
+    "red": {"prev": "green", "next": "yellow"},
+    "yellow": {"prev": "red", "next": "blue"}
+}
 
 
 def hue_saturation_from_rgb(r, g, b):
@@ -81,9 +90,10 @@ def get_on_ws_open_callback():
         setup()
 
         def run(*args):
-            curr_color = prev_color = " "
+            curr_color = prev_color = send_color = prev_send_color = ""
             color_change_time = 0
             last_send_time = 0
+            rotation = 0
             switched_color = False
             waited_2_seconds = False
             try:
@@ -96,35 +106,43 @@ def get_on_ws_open_callback():
                     norm_red = normalize_frequency(red_frequency)
                     norm_blue = normalize_frequency(blue_frequency)
                     norm_green = normalize_frequency(green_frequency)
-
                     hue, saturation = hue_saturation_from_rgb(norm_red, norm_green, norm_blue)
+                    if 320 < hue < 360 and curr_color != "red":
+                        curr_color = "red"
+                        color_change_time = curr_time
+                    elif 120 < hue < 180 and curr_color != "green":
+                        curr_color = "green"
+                        color_change_time = curr_time
+                    elif 200 < hue < 240 and curr_color != "blue":
+                        curr_color = "blue"
+                        color_change_time = curr_time
+                    elif 10 < hue < 50 and curr_color != "yellow":
+                        curr_color = "yellow"
+                        color_change_time = curr_time
 
-                    if 320 < hue < 360 and prev_color != "blue" and temp_color != "red":
-                        temp_color = "red"
-                        color_change_time = curr_time
-                    elif 120 < hue < 180 and prev_color != "yellow" and temp_color != "green":
-                        temp_color = "green"
-                        color_change_time = curr_time
-                    elif 200 < hue < 240 and prev_color != "red" and temp_color != "blue":
-                        temp_color = "blue"
-                        color_change_time = curr_time
-                    elif 10 < hue < 50 and prev_color != "green" and temp_color != "yellow":
-                        temp_color = "yellow"
-                        color_change_time = curr_time
-
-                    if curr_time - color_change_time >= 0.06:
-                        curr_color = temp_color
-                        switched_color = True
+                    if curr_time - color_change_time >= 0.06 \
+                        and (prev_send_color in COLORS.get(curr_color, dict()).values() \
+                        or not prev_send_color):
+                        send_color = curr_color
+                        
+                    if send_color and COLORS[send_color]["prev"] == prev_send_color:
+                        rotation += 1
+                    elif send_color and COLORS[send_color]["next"] == prev_send_color:
+                        rotation -= 1
 
                     waited_2_seconds = curr_time - color_change_time >= 2
-                
+                    waited_5_seconds = curr_time - color_change_time >= 5
                     if curr_time - last_send_time >= 0.1:
-                        last_send_time = curr_time
-                        switched_color = False
-                        ws.send(f'{"color": "{curr_color}", "switched": "{switched_color}", "didnt switch": "{waited_2_seconds}"}')
+                        data = json.dumps({"color": send_color, "rotation": rotation, "waited2": waited_2_seconds, "waited5": waited_5_seconds})
+                        ws.send(json.dumps({"type": "WL", "data": data}))
+                        rotation = 0
+                        if send_color != prev_send_color:
+                            print(f"sending: {send_color}")
+                        
+                        prev_send_color = send_color
 
-                    if curr_color != prev_color:
-                        print(curr_color)
+                    #if curr_color != prev_color:
+                        #print(curr_color)
 
                     prev_color = curr_color
                     
